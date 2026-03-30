c60dc98b1cb2ba60a6e43fcccffee773096fddbe
UserAdmin0 <yagyansh0711@gmail.com>
2026-03-30 19:29:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-# Simple Secure Bank App
+from flask import Flask, request
 
-print("Welcome to Secure Bank App")
+app = Flask(__name__)
 
-username = input("Enter username: ")
-password = input("Enter password: ")
+@app.route('/')
+def home():
+    return "Welcome to Secure Bank App"
 
-if username == "admin" and password == "1234":
-    print("Login Successful")
-    print("Your balance is ₹10,000")
-else:
-    print("Invalid Credentials")
+@app.route('/login', methods=['GET'])
+def login():
+    username = request.args.get('username')
+    password = request.args.get('password')
+
+    if username == "admin" and password == "1234":
+        return "Login Successful - Balance: 10,000"
+    else:
+        return "Invalid Credentials"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
